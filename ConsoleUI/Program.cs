f435bb3dd6e5018ad774cb9184791b5ab5cdb00b
enessamancioglu <enessamancioglu35@gmail.com>
2023-11-10 11:20:55 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using Entity.Concretes;
+using System.Net.Http.Headers;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+
+            product1.Id = 1;
+            product1.Name = "Pencil";
+            product1.UnitPrice = 25;
+
+            Console.WriteLine(product1.Id+product1.Name+product1.UnitPrice);
+
+
+        }
+    }
+}
